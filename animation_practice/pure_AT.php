3d66d36f18faaa784bf9127ea451387010b0f27a
peyen <53611023+PEY0314@users.noreply.github.com>
2022-07-15 15:12:45 +0800

    Update pure_AT.php

--- animation_practice/pure_AT.php
+++ animation_practice/pure_AT.php
@@ -3,7 +3,7 @@
           <div class="col-12">
             <div class="image_wrap AT_bigger">
               <div class="image_box">
-              	<img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1053&q=80" alt="image" class="image_self image_cover">
+              	<img src="img/beach.jpg" alt="image" class="image_self image_cover">
               </div>
             </div>
           </div>
