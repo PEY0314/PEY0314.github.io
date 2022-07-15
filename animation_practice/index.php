958f092b092e7a3fb5c120fdcec365e21ec1b57b
peyen <53611023+PEY0314@users.noreply.github.com>
2022-07-15 15:11:49 +0800

    Update index.php

--- animation_practice/index.php
+++ animation_practice/index.php
@@ -21,7 +21,91 @@
       
       <?php include_once "pure_AT.php"?>
       
-      <?php include_once "scroll_AT.php" ?>
+      <!-- Scroll then Animation -->
+      <div class="text-center" style="padding-top: 5%;">
+        <h1>Scroll then Animation</h1>
+      </div>
+    
+    <!-- slide -->
+      <div class="row">
+        <?php for ($i=0; $i < 2; $i++) { ?>
+        <div class="col-md-6">
+          <div class="scroll_immed_AT scroll_AT_trigger_4 AT_slideright" style="padding: 5% 0;">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. 
+          </div>
+        </div>
+        <?php } ?>
+      </div>
+
+    <!-- rotate -->
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <div class="image_wrap scroll_immed_AT scroll_AT_trigger_4 AT_rotate" style="padding: 5% 5% 10% 5%;">
+            <div class="image_box">
+              <img src="img/beach.jpg" class="image_self image_cover">
+            </div>  
+          </div>
+        </div>
+      </div>
+
+<!-- Scroll with Animation -->
+      <div class="text-center" style="padding-top: 5%;">
+        <h1>Scroll with Animation</h1>
+      </div>
+  <!-- tilt -->
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <div class="card_wrap scroll_AT_trigger_4 AT_tilt" style="padding: 5%;">
+            <div class="card_box">
+              <div class="card_overlay_front" style="background-image: url('img/cats.jpg');">
+                <div class="card_overlay_front_content">
+                  front
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  <!-- flip -->
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <div class="card_wrap scroll_AT_trigger_4 AT_flip" style="padding: 5%;">
+            <div class="card_box ">
+              <div class="card_overlay_front" style="background-image: url('img/cats.jpg');">
+                <div class="card_overlay_front_content">
+                  front
+                </div>
+                
+              </div>
+              <div class="card_overlay_back">
+                <div class="card_overlay_back_content">
+                  back
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- small size to original size -->
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <div class="image_wrap scroll_AT_trigger_4 AT_sm_to_o">
+            <div class="image_box">
+              <img src="img/beach.jpg" alt="image" class="image_self image_cover">
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- AT_pictures -->
+      <div class="row justify-content-center">
+        <div class="col-8">
+          <div class="image_wrap scroll_AT_trigger_9 AT_pictures" style="padding: 5%;">
+            <div class="image_box"> 
+              <img src="img/water.jpg" class="image_self" style="object-position: 50% 20%;">
+            </div>
+          </div>
+        </div>
+      </div>
 
       <!-- Hover -->
       <div class="text-center" style="padding-top: 5%;">
